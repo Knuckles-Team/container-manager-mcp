a8fdf0281fdebe264b3ff2e0a559134afb0e5628
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-27 01:46:20 -0600

    Bump version: 1.3.17 → 1.3.18

--- container_manager_mcp/container_manager.py
+++ container_manager_mcp/container_manager.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import platform
 import traceback
 
-__version__ = "1.3.17"
+__version__ = "1.3.18"
 
 try:
     import docker
--- container_manager_mcp/mcp.py
+++ container_manager_mcp/mcp.py
@@ -31,7 +31,7 @@ from agent_utilities.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "1.3.17"
+__version__ = "1.3.18"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
