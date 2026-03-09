a76f8ae102f4c321d1c562d1a004ff40cedf3cb3
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-08 21:31:19 -0500

    Bump version: 1.3.32 → 1.3.33

--- container_manager_mcp/mcp.py
+++ container_manager_mcp/mcp.py
@@ -19,7 +19,7 @@ from agent_utilities.mcp_utilities import (
     config,
 )
 
-__version__ = "1.3.32"
+__version__ = "1.3.33"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
